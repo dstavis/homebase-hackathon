e5c2afd2248b69983c6ba43d26b9f2d399d7aca9
David Stavis <davidstavis@gmail.com>
2014-06-07 18:05:41 -0700

    Only shows listing partials if there are listings in the database

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -18,15 +18,10 @@ post '/listings' do
 end
 
 post '/listings/:id' do
-	p params
-	p "yayyy" if params[:upvote]
-	p "noooo" if params[:downvote]
-
 	listing = Listing.find(params[:id])
 	listing.vote_num += 1 if params[:upvote]
 	listing.vote_num -= 1 if params[:downvote]
 	listing.save
-	p Listing.find(params[:id])
 	redirect '/'
 end
 
